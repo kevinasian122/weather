b62cdbbd9e4153f2a1ff945d64b55ac238f94c8c
kevinasian122 <yanchenkai@gmail.com>
2022-11-06 21:29:50 -0500

    fetched data from weather API, process into object, displayed data on the browser.

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+async function getWeather(location){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=b1f049f82b24f0b013d420e76a5f87a3`, {mode: 'cors'});
+    const data = await response.json();
+    const processedWeather = processData(data);
+    displayData(processedWeather);
+}
+getWeather("Waterloo");
+
+function processData(data){
+    const dataObject = {
+        temp: Math.round(data.main.temp - 273),
+        feels_like: Math.round(data.main.feels_like - 273),
+        maxtemp: Math.round(data.main.temp_max - 273),
+        mintemp: Math.round(data.main.temp_min - 273),
+        wind: data.wind.speed,
+        description: data.weather[0].description,
+        location: data.name,
+        country: data.sys.country
+    };
+    return dataObject;
+}
+
+const form = document.querySelector(".form");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = document.querySelector(".location").value;
+    getWeather(location);
+});
+
+function displayData(weather){
+    const content = document.querySelector(".content");
+    content.innerHTML = `
+    <div class ="name">${weather.location}, ${weather.country}</div>
+    <div class ="temp">${weather.temp}&#8451;</div>
+    <div class ="description">${weather.description}</div>
+    <div class ="range">Max: ${weather.maxtemp}&#8451; Min: ${weather.mintemp}&#8451;</div>
+    <div class ="wind">Wind: ${weather.wind} mph</div>`;
+
+}
